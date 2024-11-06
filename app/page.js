7785cd85390a812182a73b19ac1e572944b67073
winstonmeq <winstonmequila@gmail.com>
2024-11-06 09:38:48 +0800

    install shadcn

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
  <div>
-  nothing here...
+  <Button>Sample Button</Button>
+  <h1>nothing here...</h1>
  </div>
   );
 }

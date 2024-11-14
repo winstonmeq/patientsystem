9b69e353012e344a1c1090439d8ef523602eedff
winstonmeq <winstonmequila@gmail.com>
2024-11-14 15:59:32 +0800

    add env.local file

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,8 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
   //baseURL: "http://localhost:3000" 
+
   baseURL: "https://patientsystem.vercel.app/api/auth"
+  
   // the base url of your auth server
 })

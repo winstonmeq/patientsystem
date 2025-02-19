58a2f42740802c55822fd5b38945ae877a8a50ba
winstonmeq <winstonmequila@gmail.com>
2025-02-19 11:21:45 +0800

    update auth-clients

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000/api/auth" 
+  baseURL: process.env.BETTER_AUTH_URL,
 
   //baseURL: "https://patientsystem.vercel.app/api/auth"
   
